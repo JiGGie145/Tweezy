1708ba716cd2c2b3800f0e115fad4f6a1fda010e
agarwalrounak <rounakr.ag73@gmail.com>
2018-12-12 21:27:59 +0530

    Make code PEP 8 compliant

--- Twitter/checkContent.py
+++ Twitter/checkContent.py
@@ -1,22 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import re, math
+import re
+import math
 from collections import Counter
-import urllib, sys, bs4
+import urllib
+import sys
+import bs4
+import requests
+import os
 from pyshorteners import Shortener
 try:
 	from .url import fetch_url
 except:
 	from url import fetch_url
-import os
+
 dirpath = os.getcwd()+'/Twitter/'
-import requests
+
 
 class UrlExpand:
 	def __init__(self):
 		self.shortener = Shortener('Tinyurl')
-	def decodeURL(self,url):
+
+	def decodeURL(self, url):
 		try:
 			result = self.shortener.expand(url)
 			return result
@@ -24,34 +30,32 @@ class UrlExpand:
 			return url
 
 
-
 def checkAdultContent(dataset):
-	adultContentDataset=None
+	adultContentDataset = None
 	try:
 		adultContentDataset = pd.read_csv(dirpath+'adultcontenturl.csv')
 	except:
 		adultContentDataset = pd.read_csv('adultcontenturl.csv')
-	adultContentDataset = adultContentDataset.iloc[0:3,0].values
+	adultContentDataset = adultContentDataset.iloc[0:3, 0].values
 	urlExpand = UrlExpand()
 
-	if(len(dataset) == 0):
+	if len(dataset) == 0:
 		return 0
 	for data in dataset:
-		urls=fetch_url(data)
+		urls = fetch_url(data)
 		for url in urls:
 			try:
-				r=requests.get(url)
-				url=r.url
-				if(url=="https://t.co/"):
+				r = requests.get(url)
+				url = r.url
+				if url == "https://t.co/":
 					continue
 				print("checking url : "+url)
 				result = urlExpand.decodeURL(url)
-				if(result in adultContentDataset):
-					#returns 10, if adult content is present
+				if result in adultContentDataset:
+					# returns 10, if adult content is present
 					return 10
 			except:
 				print("Invalid url")
 
-
-	#returns 0, if adult content isn't present
+	# returns 0, if adult content isn't present
 	return 0
